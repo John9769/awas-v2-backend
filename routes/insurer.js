0fa0ddd487a7c43a2935d0b63f3dc124937aa97a
John9769 <piyan6979@gmail.com>
2026-06-25 18:50:28 +0800

    fixing BE files

--- routes/insurer.js
+++ routes/insurer.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const insurerController = require('../controllers/insurerController');
 const insurerAuth = require('../middleware/insurerAuth');
+const { uploadCsv } = require('../middleware/upload');
 
 // All routes protected by insurer JWT
 router.use(insurerAuth);
@@ -11,5 +12,7 @@ router.get('/drivers', insurerController.getMyDrivers);
 router.get('/writs', insurerController.getMyWrits);
 router.get('/writs/:writNumber', insurerController.getWritDetail);
 router.get('/invoices', insurerController.getMyInvoices);
+router.post('/csv-upload', uploadCsv, insurerController.uploadCsv);
+router.get('/csv-uploads', insurerController.getCsvUploads);
 
 module.exports = router;
